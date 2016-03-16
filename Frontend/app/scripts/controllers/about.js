f3aaf86e85ad76c557be275e4941e903819ef6fa
albobkov <albobkov@mera.ru>
2016-03-16 14:04:52 +0300

    add frontend angular basic frame

---
+++ Frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taskViewSampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the taskViewSampleApp
+ */
+angular.module('taskViewSampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
