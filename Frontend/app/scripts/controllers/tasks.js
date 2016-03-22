8698530b0a4f3c69919f219f59d77c9f1b652cb2
albobkov <albobkov@mera.ru>
2016-03-22 14:37:14 +0300

    grid css fixes

--- Frontend/app/scripts/controllers/tasks.js
+++ Frontend/app/scripts/controllers/tasks.js
@@ -38,6 +38,7 @@ angular.module('taskViewSampleApp')
       columnDefs: $scope.columnDef,
       enableColumnMenus: false,
       rawData: false,
+      enableSorting: true,
       enablePaging: true,
       paginationPageSizes: [25, 50, 75],
       paginationPageSize: 25,
@@ -68,13 +69,21 @@ angular.module('taskViewSampleApp')
   };
   $scope.columnDef = [
     {
+      field: 'test',
+      displayName: '',
+      visible: true,
+      minWidth: 100,
+      maxWidth: 100
+    },
+    {
       field: 'task_group.operator.name',
       displayName: "Operator",
-      visible: true},
+      visible: true,
+    },
     {
       field: 'description',
       displayName: "Description",
-      visible: true
+      visible: true 
     },
     { field: 'task_group.priority',
       displayName: "Priority",
